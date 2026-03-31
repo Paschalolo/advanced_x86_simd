c72ecb5f404962f8c4c8472f110af956ed5a5d0b
Paschalolo <pastechnology1@gmail.com>
2026-03-30 19:41:37 -0500

    mesas

--- src/a1.cpp
+++ src/a1.cpp
@@ -10,9 +10,11 @@ void __attribute__((noinline))calcz_cpp(float* z ,const float * x , const float*
 // make sure data is aligned at 64 byte boundary 
 void __attribute__((noinline))calcz_avx1(float* z ,const float * x , const float* y ,size_t n){
 	size_t i = 0 ; 
-	for(; (n-i) >= 8 ; i += 8){
-		__m256 total = 	 _mm256_add_ps (_mm256_load_ps (x),_mm256_load_ps (y) );
-		  _mm256_stream_ps (z,total);
+	for(; (n-i) >= 16; i += 16){
+		__m256 total = 	 _mm256_add_ps (_mm256_load_ps (x+i),_mm256_load_ps (y+i) );
+		__m256 total1 = 	 _mm256_add_ps (_mm256_load_ps (x+i),_mm256_load_ps (y+i) );
+		  _mm256_stream_ps (z+i,total);
+		  _mm256_stream_ps (z + (i*2),total1);
 	}
 
 	for(;i < n; ++i ){
@@ -27,13 +29,13 @@ extern "C" void calcx_avx2(float* z ,const float * x , const float* y ,size_t n)
 __attribute__((aligned(64))) float ans[20]{0}; 
 // Testing the function 
 int main(){
-	__attribute__((aligned(64))) float m_data[20] {
+	__attribute__((aligned(32))) float m_data[20] {
 		1.0f , 2.0f , 3.0f , 4.0f , 5.0f ,
 		6.0f ,7.0f , 8.0f , 9.0f , 10.0f , 
 		11.0f  ,12.0f , 13.0f , 14.9f , 154.4f,
 		16.3f, 17.23f , 18.34f, 19.33f , 20.4f
 	};
-	__attribute__((aligned(64))) float m_data1[20] {
+	__attribute__((aligned(32))) float m_data1[20] {
 		1.0f , 2.0f , 3.0f , 4.0f , 5.0f ,
 		6.0f ,7.0f , 8.0f , 9.0f , 10.0f , 
 		11.0f  ,12.0f , 13.0f , 14.9f , 154.4f,
