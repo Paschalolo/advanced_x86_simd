8d9279e8437eb29251fefa57f03c8cd3d3cc3e7b
Paschalolo <pastechnology1@gmail.com>
2026-04-02 17:34:59 -0500

    update x86 sim DDD

--- src/pixel_clipping.cpp
+++ src/pixel_clipping.cpp
@@ -21,6 +21,8 @@ struct ClipData{
 bool check_args(ClipData* ){return false;}
 
 
+// Too long 
+
 void __attribute__((used)) ClipPixels_avx2(ClipData* clipdata){
 	if(check_args(clipdata))[[unlikely]]{
 		fprintf(stderr, "Failed data");
@@ -30,29 +32,45 @@ void __attribute__((used)) ClipPixels_avx2(ClipData* clipdata){
 	auto pb_des = clipdata->m_PbDes; 
 	auto num_pixels = clipdata->m_Num_pixels; 
 	size_t num_clipped_pixels = 0;
-	uint64_t thresh_lo = static_cast<uint64_t>(clipdata->m_threshold);
-	uint64_t thresh_hi = static_cast<uint64_t>(clipdata->m_thresh_hi);
+	uint8_t thresh_lo{clipdata->m_threshold};
+	uint8_t	thresh_hi{clipdata->m_thresh_hi};
 	// main for loop
 	size_t i = 0 ; 
-	__m256i mask_lo =  _mm256_broadcastb_epi8( _mm_cvtsi64_si128(std::bit_cast<long long>(thresh_lo)));
-	__m256i mask_hi =  _mm256_broadcastb_epi8(_mm_cvtsi64_si128(std::bit_cast<long long>(thresh_hi)));
-	__m256i all_ones = _mm256_cmpeq_epi32(_mm256_setzero_si256(), _mm256_setzero_si256());
-	// load loop to registers 
-	for( ; (num_pixels - i) > 32; i += 32 ){
+	__m256i mask_lo = _mm256_set1_epi8(static_cast<char>(thresh_lo));
+	__m256i mask_hi = _mm256_set1_epi8(static_cast<char>(thresh_hi));
+	// load loop to registers
+	__m256i dest_reg=   _mm256_load_si256(reinterpret_cast<__m256i*>(&(pbsrc[i])));
+	__m256i dest_reg1=   _mm256_load_si256(reinterpret_cast<__m256i*>(&(pbsrc[i+32])));
+	__m256i dest_reg2=   _mm256_load_si256(reinterpret_cast<__m256i*>(&(pbsrc[i+64])));
+	for( ; (num_pixels - i) >= 96; i += 96){
 		// load rgiusters
-		__m256i dest_reg=   _mm256_load_si256 (std::bit_cast<__m256i*>(pbsrc));
-		__m256i m_thres_high =  _mm256_max_epu8 (dest_reg , mask_hi ); // greater than  
-		__m256i m_thres_low = _mm256_min_epu8 ( dest_reg, mask_lo); // less than
-		__m256i new__ = _mm256_xor_si256(_mm256_or_si256 ( m_thres_low, m_thres_high), all_ones); 
-		int a = _mm256_movemask_epi8 ( new__); 
-		__m256i pipe1 = _mm256_and_si256(_mm256_xor_si256(_mm256_or_si256 ( m_thres_low, m_thres_high), all_ones), dest_reg);
-		m_thres_high =  _mm256_and_si256 ( dest_reg, m_thres_high);
-		m_thres_low = _mm256_and_si256 ( dest_reg, m_thres_low);
-		 pipe1 = _mm256_or_si256(m_thres_high,  _mm256_or_si256(m_thres_low , pipe1));
-		num_clipped_pixels += (32- static_cast<size_t>(__builtin_popcount(std::bit_cast<unsigned int>(a))));
+		__m256i temp1 =  _mm256_max_epu8 (dest_reg , mask_lo ); // greater than  
+		__m256i temp2 =  _mm256_max_epu8 (dest_reg1 , mask_lo ); // greater than  
+		__m256i temp3 =  _mm256_max_epu8 (dest_reg2, mask_lo ); // greater than  
+		__m256i des_val = _mm256_min_epu8 ( temp1,mask_hi); // less than
+		__m256i des_val2 = _mm256_min_epu8 ( temp2,mask_hi); // less than
+		__m256i des_val3 = _mm256_min_epu8 ( temp3,mask_hi); // less than
+		 _mm256_stream_si256 (std::bit_cast<__m256i*>(&(pb_des[i])),des_val);
+		 _mm256_stream_si256 (std::bit_cast<__m256i*>(&(pb_des[i+32])),des_val2);
+		 _mm256_stream_si256 (std::bit_cast<__m256i*>(&(pb_des[i+64])),des_val3);
+		 __m256i temp12 =_mm256_cmpeq_epi8(dest_reg , des_val);
+		 __m256i temp13=_mm256_cmpeq_epi8(dest_reg , des_val);
+		 __m256i temp14=_mm256_cmpeq_epi8(dest_reg , des_val);
+
+		 int a = _mm256_movemask_epi8(temp12);
+		 int b = _mm256_movemask_epi8(temp13);
+		 int c = _mm256_movemask_epi8(temp14);
+
+		unsigned int sum = std::bit_cast<unsigned int>(~a) + std::bit_cast<unsigned int>(~b) + std::bit_cast<unsigned int>(~c) ;  
+		num_clipped_pixels += (96 -sum);
+		if( i +96 + 95 < num_pixels){
+	
+		 dest_reg=   _mm256_load_si256(reinterpret_cast<__m256i*>(&(pbsrc[i+96])));
+	 	dest_reg1=   _mm256_load_si256(reinterpret_cast<__m256i*>(&(pbsrc[i+128])));
+		 dest_reg2=   _mm256_load_si256(reinterpret_cast<__m256i*>(&(pbsrc[i+160])));
+		}
 		// compare for equal 
 		// store in the cache bypass cache because data isnt used again
-		 _mm256_stream_si256 (std::bit_cast<__m256i*>(&(pb_des[i])),pipe1);
 	}
 	// finsh small values 
 	for(; i < num_pixels ; i++){
