250f7e720beaecb539037b421a1bde031d0eb417
Paschalolo <pastechnology1@gmail.com>
2026-04-03 13:07:19 -0500

    update x86 sim DDD

--- src/least_sqaure.cpp
+++ src/least_sqaure.cpp
@@ -34,9 +34,8 @@ void Calc_least_square_avx( double*  m,  double* b , const double* x , const dou
 	*b = 0.0 ; 
 	if(!checkArgs(x,y,n))[[unlikely]]  exit(1);
 	double zn = static_cast<double>(n);
-	double sum_x{0.0} , sum_y{0.0}, sum_xx{0.0}, sum_xy{0.0};
 	size_t i {0};
-	__m256d temp1 , tempy1 ,temp_mul2 , temp_mulxy2;
+	__m256d temp1{} , tempy1{} ,temp_mul2{} , temp_mulxy2{};
 	for( ; (n-i) >= 4; i+= 4 ){
 		// load 
 		__m256d sum_x_P = _mm256_load_pd (x);
@@ -59,12 +58,18 @@ void Calc_least_square_avx( double*  m,  double* b , const double* x , const dou
 		__m256d temp_mulxy1 = _mm256_add_pd (std::bit_cast<__m256d>(_mm256_bsrli_epi128(std::bit_cast<__m256i>(mul_xy),16)), mul_xy);
 		temp_mulxy2 = _mm256_add_pd (std::bit_cast<__m256d>(_mm256_bsrli_epi128(std::bit_cast<__m256i>(temp_mulxy1),8)),temp_mulxy1 );
 	}
-	for(; i < n ;i++){}
-	double denom ; 
-	double tempo{0} ;
+	//double sum_xx  = , sum_y , sum_xx sum_xy;
+	double sum_x{temp1[0]} , sum_y{tempy1[0]}, sum_xx{temp_mul2[0]}, sum_xy{temp_mulxy2[0]};
+	for(; i < n ;i++){
+		
+		sum_x += x[i];
+		sum_y += y[i];
+		sum_xx += x[i] * x[i];
+		sum_xy += x[i] * y[i];
+	}
 
-	//double denom = (zn * sum_xx) - (sum_x * sum_x);
-	__asm__ volatile(
+	double denom = (zn * sum_xx) - (sum_x * sum_x);
+	/* __asm__ volatile(
 			".intel_syntax noprefix\n\t"
 			"vmulsd %0 , %1 , %x2\n\t"
 			"vmulsd %4 , %x3 , %x3\n\t"
@@ -73,7 +78,7 @@ void Calc_least_square_avx( double*  m,  double* b , const double* x , const dou
 				: "+x"(denom)
 				: "x" (zn), "x"(temp_mul2) , "x"(temp1), "x"(tempo)
 				:"cc"
-			);
+			);*/ 
 	if(fabs(denom) >= c_LsEpsilon){
 		*m = (zn * sum_xy - sum_x * sum_y)/ denom;
 		*b = (sum_xx * sum_y - sum_x * sum_xy)/ denom;
