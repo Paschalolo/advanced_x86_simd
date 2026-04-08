6c9c5da8834ff30099b7239e3a998eeb12a84095
Paschalolo <pastechnology1@gmail.com>
2026-04-08 10:41:03 -0500

    update x86 sim DDD

--- src/tt3a.asm
+++ src/tt3a.asm
@@ -13,4 +13,20 @@ sum_array:
 	loop
 	ret 
 
- 	
+; void calcu_array (int64_t* c , const int64_t* a , const int64_t* b , size_t n );
+; c[i] = (a[i] * 25 ) / (b[i] + 10)
+calcu_array:
+	; rcx cpontains loop count 
+	mov r8 , 0 
+	mov r10 , rdi 
+	mov rdi ,  rdx 
+.sub_loop: 
+	imul r9 , qword[rsi + r8 * 8 ], 25 
+	mov rax , qword[rdi + r8 * 8] 
+	add rax , 10 
+	idiv r9
+	mov qword[r10 + r8 * 8],rax
+	inc r8
+	loop 
+	xor rax , rax 
+	ret 
